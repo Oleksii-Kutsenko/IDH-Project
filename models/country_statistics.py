e6a871e104eb217bfc501ec68223743000a5dc4a
Oleksii Kutsenko <kutsenko.new@gmail.com>
2024-06-21 20:46:18 +0200

    New lines fix

--- models/country_statistics.py
+++ models/country_statistics.py
@@ -16,4 +16,4 @@ class CountryStatistics(Base):
     trade_id = Column(Integer, ForeignKey("trade_dimension.trade_id"))
     # financial_center_id = Column(Integer, ForeignKey("financial_center_dimension.financial_center_id"))
     economic_id = Column(Integer, ForeignKey("economic_dimension.economic_id"))
-    # reserve_currency_id = Column(Integer, ForeignKey("reserve_currency_dimension.reserve_currency_id"))       
+    # reserve_currency_id = Column(Integer, ForeignKey("reserve_currency_dimension.reserve_currency_id"))
--- models/military_dimension.py
+++ models/military_dimension.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, Float
+from sqlalchemy import Column, Float, Integer
 
 from .base import Base
 
